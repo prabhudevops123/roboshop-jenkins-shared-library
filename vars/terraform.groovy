0d032f196fe7737cb52125da5dce2c143d757ce9
Prabhu <prabh.rao@gmail.com>
2023-04-04 11:53:56 +0530

    roboshop docdb jenkins

---
+++ vars/terraform.groovy
@@ -0,0 +1,32 @@
+def call() {
+    pipeline {
+        agent any
+
+        parameters {
+            string(name: 'ENV', defaultValue: '', description: 'Which Environment?')
+            string(name: 'ACTION', defaultValue: '', description: 'Which Action?')
+        }
+
+        options {
+            ansiColor('xterm')
+        }
+
+        stages {
+
+            stage('Init') {
+                steps {
+                    sh 'terraform init -backend-config=env-${ENV}/state.tfvars'
+                }
+            }
+
+            stage('Apply') {
+                steps {
+                    //sh 'terraform ${ACTION} -auto-approve -var-file=env-dev/main.tfvars'
+                    sh 'echo'
+                }
+            }
+
+        }
+
+    }
+}
