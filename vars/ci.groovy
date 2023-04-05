38052657588f05570e46186b849191f312245eec
Prabhu <prabh.rao@gmail.com>
2023-04-05 19:26:58 +0530

    roboshop docdb jenkins

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -6,28 +6,52 @@ def call() {
 
             stage('Compile/Build') {
                 steps {
-                    script {
-                        common.compile()
-                    }
+                    echo 'Compile/Build'
+
                 }
             }
-
             stage('Test Cases') {
                 steps {
-                    script {
-                        common.testcases()
-                    }
-                }
-            }
-
-            stage('Code Quality') {
-                steps {
-                    script {
-                        common.codequality()
-                    }
+                    echo 'Test Cases'
                 }
             }
         }
-
     }
 }
+
+
+
+
+//def call() {
+//    pipeline {
+//        agent any
+//
+//        stages {
+//
+//            stage('Compile/Build') {
+//                steps {
+//                    script {
+//                        common.compile()
+//                    }
+//                }
+//            }
+//
+//            stage('Test Cases') {
+//                steps {
+//                    script {
+//                        common.testcases()
+//                    }
+//                }
+//            }
+//
+//            stage('Code Quality') {
+//                steps {
+//                    script {
+//                        common.codequality()
+//                    }
+//                }
+//            }
+//        }
+//
+//    }
+//}
