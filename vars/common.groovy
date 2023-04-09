c75c30469a1cbc826c4204b38a1ba1a6dc3e2616
Prabhu <prabh.rao@gmail.com>
2023-04-09 17:06:37 +0530

    roboshop docdb jenkins

--- vars/common.groovy
+++ vars/common.groovy
@@ -15,16 +15,16 @@ def testcases() {
     sh 'echo OK'
 }
 
-//def codequality() {
-//    withAWSParameterStore(credentialsId: 'PARAM1', naming: 'absolute', path: '/sonarqube', recursive: true, regionName: 'us-east-1') {
-//        //sh 'sonar-scanner -Dsonar.host.url=http://172.31.6.76:9000 -Dsonar.login=${SONARQUBE_USER} -Dsonar.password=${SONARQUBE_PASS} -Dsonar.projectKey=${component} ${sonar_extra_opts}'
-//        sh 'echo OK'
-//    }
-
 def codequality() {
     withAWSParameterStore(credentialsId: 'PARAM1', naming: 'absolute', path: '/sonarqube', recursive: true, regionName: 'us-east-1') {
-        sh 'sonar-scanner -Dsonar.host.url=http://172.31.6.76:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.projectKey=${component} ${sonar_extra_opts}'
+        sh 'sonar-scanner -Dsonar.host.url=http://172.31.6.76:9000 -Dsonar.login=${SONARQUBE_USER} -Dsonar.password=${SONARQUBE_PASS} -Dsonar.projectKey=${component} ${sonar_extra_opts}'
+        sh 'echo OK'
     }
-}
+
+//def codequality() {
+//    withAWSParameterStore(credentialsId: 'PARAM1', naming: 'absolute', path: '/sonarqube', recursive: true, regionName: 'us-east-1') {
+//        sh 'sonar-scanner -Dsonar.host.url=http://172.31.6.76:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.projectKey=${component} ${sonar_extra_opts}'
+//    }
+//}
 
 //-Dsonar.qualitygate.wait=true
