e2b2c0739d12da00a48ad0d4fcf796ed6bc24fa1
Prabhu <prabh.rao@gmail.com>
2023-04-09 17:47:45 +0530

    roboshop docdb jenkins

--- vars/common.groovy
+++ vars/common.groovy
@@ -25,6 +25,5 @@ def codequality() {
 //    withAWSParameterStore(credentialsId: 'PARAM1', naming: 'absolute', path: '/sonarqube', recursive: true, regionName: 'us-east-1') {
 //        sh 'sonar-scanner -Dsonar.host.url=http://172.31.6.76:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.projectKey=${component} ${sonar_extra_opts}'
 //    }
+    // -Dsonar.qualitygate.wait=true
 //}
-
-//-Dsonar.qualitygate.wait=true
